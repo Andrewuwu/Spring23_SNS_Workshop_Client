b2559cd13969292ca3184ea0145529e3a474e82e
Andrew Mao <maosteram@gmail.com>
2024-03-28 16:41:25 -0500

    added register and login forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './components/common/NavColumn';
 import MainFeedContainer from './components/common/MainFeedContainer';
+import Register from './components/login/Register';
 
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainFeedContainer/>}/>
-        <Route path="/login" element={<div></div>} />
-        <Route path="/profile" element={<div></div>} />
+        <Route path="/login" element={<div>HELLOWORLD</div>} />
+        <Route path="/register" element={<Register/>}/>
         <Route path="/contact" element={<div></div>} />
         <Route path="/otherstuff" element={<div></div>} />
       </Routes>
---
+++ src/components/login/login.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {useState} from "react";
+import {FormControl, FormLabel, Input, FormHelperText, FormErrorMessage} from '@chakra-ui/react';
+import "./login.css";
+
+
+function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleEmailChange = (e) => setEmail(e.target.value);
+    const handlePasswordChange = (e) => setPassword(e.target.value);
+    
+    const isError = email === '';
+
+    return(
+        <div className="center">
+            <FormControl isInvalid={isError}>
+                <FormLabel>Email</FormLabel>
+                <Input type='email' value={email} onChange={handleEmailChange} />
+                {!isError ? (
+                <FormHelperText>
+                    Enter the email you'd like to receive the newsletter on.
+                </FormHelperText>
+                ) : (
+                <FormErrorMessage>Email is required.</FormErrorMessage>
+                )}
+                <FormLabel>Password</FormLabel>
+                <Input type='password' value={password} onChange={handlePasswordChange} />
+                {!isError ? (
+                <FormHelperText>
+                    Enter Your Password
+                </FormHelperText>
+                ) : (
+                <FormErrorMessage>Password is Required</FormErrorMessage>
+                )}
+            </FormControl>
+        </div>
+    );
+}
+
+export default Login;
